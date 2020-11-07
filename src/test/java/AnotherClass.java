d5c32f8d7d28c6d0cb495f4cb25ae65b35c8bd6f
Lmishel89 <irbistechsoft@gmail.com>
2020-11-07 13:43:48 +0200

    Added AnotherClass 07/11/20

---
+++ src/test/java/AnotherClass.java
@@ -0,0 +1,12 @@
+import org.testng.annotations.Test;
+
+public class AnotherClass {
+    @Test
+        public void Add() {
+            int a = 7;
+            int d = 9;
+
+            System.out.println(a + d);
+        }
+    }
+
